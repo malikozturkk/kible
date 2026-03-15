2375fd405d82ac6b3061dd20fa748cb90c9e39ea
Malik Öztürk <malik.ozturk@formica.ai>
2026-03-15 18:26:14 +0300

    feat: add cors enable for localhost:3001

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter.js
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
